8edc4591a5631604e4a5cc50272c07804780c1d8
Quan Trung <quantrung286@gmail.com>
2022-04-09 22:14:54 +0700

    up

--- src/App.js
+++ src/App.js
@@ -40,6 +40,74 @@ function App() {
 					</ul>
 					<footer>Add another card</footer>
 				</div>
+				<div className='column'>
+					<header>BrainStorm</header>
+					<ul>
+						<li>
+							<img
+								src='https://i.ytimg.com/vi/vnXvYy_GIGI/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCrwRTvTrUtouhuTLEKIkuezm04jw'
+								alt=''
+							/>
+							Title: Trungquandev
+						</li>
+						<li>Add what you'd to work on below</li>
+						<li>Add what you'd to work on below</li>
+						<li>Add what you'd to work on below</li>
+						<li>Add what you'd to work on below</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className='column'>
+					<header>BrainStorm</header>
+					<ul>
+						<li>
+							<img
+								src='https://i.ytimg.com/vi/vnXvYy_GIGI/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCrwRTvTrUtouhuTLEKIkuezm04jw'
+								alt=''
+							/>
+							Title: Trungquandev
+						</li>
+						<li>Add what you'd to work on below</li>
+						<li>Add what you'd to work on below</li>
+						<li>Add what you'd to work on below</li>
+						<li>Add what you'd to work on below</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className='column'>
+					<header>BrainStorm</header>
+					<ul>
+						<li>
+							<img
+								src='https://i.ytimg.com/vi/vnXvYy_GIGI/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCrwRTvTrUtouhuTLEKIkuezm04jw'
+								alt=''
+							/>
+							Title: Trungquandev
+						</li>
+						<li>Add what you'd to work on below</li>
+						<li>Add what you'd to work on below</li>
+						<li>Add what you'd to work on below</li>
+						<li>Add what you'd to work on below</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className='column'>
+					<header>BrainStorm</header>
+					<ul>
+						<li>
+							<img
+								src='https://i.ytimg.com/vi/vnXvYy_GIGI/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCrwRTvTrUtouhuTLEKIkuezm04jw'
+								alt=''
+							/>
+							Title: Trungquandev
+						</li>
+						<li>Add what you'd to work on below</li>
+						<li>Add what you'd to work on below</li>
+						<li>Add what you'd to work on below</li>
+						<li>Add what you'd to work on below</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
 			</div>
 		</div>
 	)
