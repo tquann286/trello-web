a69650a6c7a20a29649d63d90f2ff083a0d13093
Quan Trung <quantrung286@gmail.com>
2022-04-10 22:07:36 +0700

    up

---
+++ src/components/Column/Column.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import './Column.scss'
+
+function Column() {
+  return (
+    <div className='column'>
+				<header>BrainStorm</header>
+				<ul>
+					<li>
+						<img
+							src='https://i.ytimg.com/vi/vnXvYy_GIGI/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCrwRTvTrUtouhuTLEKIkuezm04jw'
+							alt=''
+						/>
+						Title: Trungquandev
+					</li>
+					<li>Add what you'd to work on below</li>
+					<li>Add what you'd to work on below</li>
+					<li>Add what you'd to work on below</li>
+					<li>Add what you'd to work on below</li>
+				</ul>
+				<footer>Add another card</footer>
+			</div>
+  )
+}
+
+export default Column
---
+++ src/components/Task/Task.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Task.scss'
+
+function Task() {
+  return (
+    <nav className='navbar-app'>App Bar</nav>
+  )
+}
+
+export default Task
