2a1a9fc35032587eaee567bbfff5dfb4f7f862ce
Quan Trung <quantrung286@gmail.com>
2022-04-10 21:51:02 +0700

    up

--- src/App.js
+++ src/App.js
@@ -4,48 +4,14 @@ import './App.scss'
 // Custom components
 import AppBar from 'components/AppBar/AppBar'
 import BoardBar from 'components/BoardBar/BoardBar'
+import BoardContent from 'components/BoardContent/BoardContent'
 
 function App() {
 	return (
 		<div className='trello-trungquandev-master'>
 			<AppBar />
 			<BoardBar />
-			<div className='board-content'>
-				<div className='column'>
-					<header>BrainStorm</header>
-					<ul>
-						<li>
-							<img
-								src='https://i.ytimg.com/vi/vnXvYy_GIGI/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCrwRTvTrUtouhuTLEKIkuezm04jw'
-								alt=''
-							/>
-							Title: Trungquandev
-						</li>
-						<li>Add what you'd to work on below</li>
-						<li>Add what you'd to work on below</li>
-						<li>Add what you'd to work on below</li>
-						<li>Add what you'd to work on below</li>
-					</ul>
-					<footer>Add another card</footer>
-				</div>
-				<div className='column'>
-					<header>BrainStorm</header>
-					<ul>
-						<li>
-							<img
-								src='https://i.ytimg.com/vi/vnXvYy_GIGI/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCrwRTvTrUtouhuTLEKIkuezm04jw'
-								alt=''
-							/>
-							Title: Trungquandev
-						</li>
-						<li>Add what you'd to work on below</li>
-						<li>Add what you'd to work on below</li>
-						<li>Add what you'd to work on below</li>
-						<li>Add what you'd to work on below</li>
-					</ul>
-					<footer>Add another card</footer>
-				</div>
-			</div>
+			<BoardContent />
 		</div>
 	)
 }
--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -4,7 +4,42 @@ import './BoardContent.scss'
 
 function BoardContent() {
   return (
-    <nav className='navbar-app'>App Bar</nav>
+    <div className='board-content'>
+				<div className='column'>
+					<header>BrainStorm</header>
+					<ul>
+						<li>
+							<img
+								src='https://i.ytimg.com/vi/vnXvYy_GIGI/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCrwRTvTrUtouhuTLEKIkuezm04jw'
+								alt=''
+							/>
+							Title: Trungquandev
+						</li>
+						<li>Add what you'd to work on below</li>
+						<li>Add what you'd to work on below</li>
+						<li>Add what you'd to work on below</li>
+						<li>Add what you'd to work on below</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+				<div className='column'>
+					<header>BrainStorm</header>
+					<ul>
+						<li>
+							<img
+								src='https://i.ytimg.com/vi/vnXvYy_GIGI/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCrwRTvTrUtouhuTLEKIkuezm04jw'
+								alt=''
+							/>
+							Title: Trungquandev
+						</li>
+						<li>Add what you'd to work on below</li>
+						<li>Add what you'd to work on below</li>
+						<li>Add what you'd to work on below</li>
+						<li>Add what you'd to work on below</li>
+					</ul>
+					<footer>Add another card</footer>
+				</div>
+			</div>
   )
 }
 
