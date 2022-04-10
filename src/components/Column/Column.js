e85d534bac00b74c13df1d25d5d1df1358904331
Quan Trung <quantrung286@gmail.com>
2022-04-10 22:17:21 +0700

    up

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,27 +1,22 @@
 import React from 'react'
 
 import './Column.scss'
+import Task from 'components/Task/Task'
 
 function Column() {
-  return (
-    <div className='column'>
-				<header>BrainStorm</header>
-				<ul>
-					<li>
-						<img
-							src='https://i.ytimg.com/vi/vnXvYy_GIGI/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCrwRTvTrUtouhuTLEKIkuezm04jw'
-							alt=''
-						/>
-						Title: Trungquandev
-					</li>
-					<li>Add what you'd to work on below</li>
-					<li>Add what you'd to work on below</li>
-					<li>Add what you'd to work on below</li>
-					<li>Add what you'd to work on below</li>
-				</ul>
-				<footer>Add another card</footer>
-			</div>
-  )
+	return (
+		<div className='column'>
+			<header>BrainStorm</header>
+			<ul className='task-list'>
+				<Task />
+				<li className='task-item'>Add what you'd to work on below</li>
+				<li className='task-item'>Add what you'd to work on below</li>
+				<li className='task-item'>Add what you'd to work on below</li>
+				<li className='task-item'>Add what you'd to work on below</li>
+			</ul>
+			<footer>Add another card</footer>
+		</div>
+	)
 }
 
 export default Column
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -3,9 +3,15 @@ import React from 'react'
 import './Task.scss'
 
 function Task() {
-  return (
-    <nav className='navbar-app'>App Bar</nav>
-  )
+	return (
+		<li className='task-item'>
+			<img
+				src='https://i.ytimg.com/vi/vnXvYy_GIGI/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCrwRTvTrUtouhuTLEKIkuezm04jw'
+				alt=''
+			/>
+			Title: Trungquandev
+		</li>
+	)
 }
 
 export default Task
