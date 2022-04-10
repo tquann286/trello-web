8a3815b97dc33d7a67f853e10268af5e403b8c7f
Quan Trung <quantrung286@gmail.com>
2022-04-11 00:44:24 +0700

    up

--- src/actions/initialData.js
+++ src/actions/initialData.js
@@ -8,10 +8,10 @@ export const initialData = {
 					id: 'column-1',
 					boardId: 'board-1',
 					title: 'To do column',
-					cardOrder: ['card-1', 'card-2', 'card-3'],
+					cardOrder: ['card-1', 'card-2', 'card-3', 'card-4', 'card-5', 'card-6', 'card-7',],
 					cards: [
 						{
-							id: 'card-1', boardId: 'board-1', columnId: 'column-1', title: 'Title of card 1', cover: null,
+							id: 'card-1', boardId: 'board-1', columnId: 'column-1', title: 'Title of card 1', cover: 'https://i.ytimg.com/vi/vnXvYy_GIGI/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLCrwRTvTrUtouhuTLEKIkuezm04jw',
 						},
             {
 							id: 'card-2', boardId: 'board-1', columnId: 'column-1', title: 'Title of card 2', cover: null,
@@ -30,6 +30,40 @@ export const initialData = {
 						},
             {
 							id: 'card-7', boardId: 'board-1', columnId: 'column-1', title: 'Title of card 7', cover: null,
+						},
+					],
+				},
+        {
+					id: 'column-2',
+					boardId: 'board-1',
+					title: 'Inprogress column',
+					cardOrder: ['card-8', 'card-9', 'card-10'],
+					cards: [
+						{
+							id: 'card-8', boardId: 'board-1', columnId: 'column-2', title: 'Title of card 8', cover: null,
+						},
+            {
+							id: 'card-9', boardId: 'board-1', columnId: 'column-2', title: 'Title of card 9', cover: null,
+						},
+            {
+							id: 'card-10', boardId: 'board-1', columnId: 'column-2', title: 'Title of card 10', cover: null,
+						},
+					],
+				},
+        {
+					id: 'column-3',
+					boardId: 'board-1',
+					title: 'Done column',
+					cardOrder: ['card-11', 'card-12', 'card-13'],
+					cards: [
+						{
+							id: 'card-11', boardId: 'board-1', columnId: 'column-3', title: 'Title of card 11', cover: null,
+						},
+            {
+							id: 'card-12', boardId: 'board-1', columnId: 'column-3', title: 'Title of card 12', cover: null,
+						},
+            {
+							id: 'card-13', boardId: 'board-1', columnId: 'column-3', title: 'Title of card 13', cover: null,
 						},
 					],
 				},
