75369f7723a4dff149ee46c94a9543ae64089540
Quan Trung <quantrung286@gmail.com>
2022-04-11 11:51:28 +0700

    up

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { isEmpty } from 'lodash'
 
 import './BoardContent.scss'
 
 import Column from 'components/Column/Column'
 
+import { initialData } from 'actions/initialData'
 
 function BoardContent() {
+	const [board, setBoard] = useState({})
+	const [columns, setColumns] = useState({})
+
+	useEffect(() => {
+		const boardFromDB = initialData.boards.find(
+			(board) => board.id === 'board-1'
+		)
+		if (boardFromDB) {
+			setBoard(boardFromDB)
+		}
+	}, [])
+
+	if (isEmpty(board)) {
+		return <div className='not-found'>Board not found.</div>
+	}
+
 	return (
 		<div className='board-content'>
 			<Column />
