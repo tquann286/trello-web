62e3b0359dff10bffb7e4732f5a8c04a9c7c6a30
Quan Trung <quantrung286@gmail.com>
2022-04-11 17:26:20 +0700

    up

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,7 +5,7 @@ import './Card.scss'
 function Card({ card }) {
 	return (
 		<div className='card-item'>
-			{card.cover && <img src={card.cover} className='card-cover' alt={card.title} />}
+			{card.cover && <img src={card.cover} className='card-cover' alt={card.title} onMouseDown={e => e.preventDefault()} />}
 			{card.title}
 		</div>
 	)
