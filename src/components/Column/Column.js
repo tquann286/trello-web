7f19d06e683e934ddf5ad320d4780288e5de6885
Quan Trung <quantrung286@gmail.com>
2022-04-11 16:45:52 +0700

    up

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -17,20 +17,20 @@ function Column({ column }) {
 			<header className='column-drag-handle'>{column.title}</header>
 			<div className='card-list'>
 				<Container
+					// onDragStart={(e) => console.log('drag started', e)}
+					// onDragEnd={(e) => console.log('drag end', e)}
+					// onDragEnter={() => {
+					// 	console.log('drag enter:', column.id)
+					// }}
+					// onDragLeave={() => {
+					// 	console.log('drag leave:', column.id)
+					// }}
+					// onDropReady={(p) => console.log('Drop ready: ', p)}
 					groupName='col'
-					onDragStart={(e) => console.log('drag started', e)}
-					onDragEnd={(e) => console.log('drag end', e)}
 					onDrop={onCardDrop}
 					getChildPayload={(index) => cards[index]}
 					dragClass='card-ghost'
 					dropClass='card-ghost-drop'
-					onDragEnter={() => {
-						console.log('drag enter:', column.id)
-					}}
-					onDragLeave={() => {
-						console.log('drag leave:', column.id)
-					}}
-					onDropReady={(p) => console.log('Drop ready: ', p)}
 					dropPlaceholder={{
 						animationDuration: 150,
 						showOnTop: true,
